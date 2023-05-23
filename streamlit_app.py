ab8132cac4898ac4686f39d3976c06caa7e797c5
Kent Moser <41020154+moserkent@users.noreply.github.com>
2023-05-23 17:23:13 -0500

    Update streamlit_app.py
    removed the additional data frame lines of code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,15 +37,6 @@ try:
 except URLError as e:
   streamlit.error()
   
-# streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
 # Don't run anything past here while we troubleshoot
 streamlit.stop()
 
